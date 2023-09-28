b3d678d1eaca9ea3ffc66a704fd416c9be142525
navodel <115118635+Navodel@users.noreply.github.com>
2023-09-28 17:35:54 -0300

    Se añade Calculadora de IMC
    Se añadió una calculadora de IMC

--- script.js
+++ script.js
@@ -138,3 +138,42 @@ function binaryToText() {
     });
     return morseArray.join(" ");
   }
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('calculator-form');
+    const resultDiv = document.getElementById('result');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        const weight = parseFloat(document.getElementById('weight').value);
+        const height = parseFloat(document.getElementById('height').value) / 100; // Convertir altura a metros
+
+        if (isNaN(weight) || isNaN(height) || height === 0) {
+            resultDiv.textContent = 'Por favor, ingresa valores válidos.';
+            return;
+        }
+
+        const bmi = weight / (height * height);
+        let message = '';
+
+        if (bmi < 18.5) {
+            message = 'Insuficiencia de peso';
+        } else if (bmi >= 18.5 && bmi < 24.9) {
+            message = 'Peso saludable';
+        } else if (bmi >= 25 && bmi < 29.9) {
+            message = 'Sobrepeso';
+        } else {
+            message = 'Obesidad';
+        }
+
+        resultDiv.textContent = `Tu IMC es: ${bmi.toFixed(2)}. ${message}`;
+    });
+});
