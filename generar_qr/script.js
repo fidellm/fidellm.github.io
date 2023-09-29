3831cd5c8ef72223d72f274bfaf1f8468b6f892f
navodel <115118635+Navodel@users.noreply.github.com>
2023-09-29 15:51:48 -0300

    Update script.js

--- generar_qr/script.js
+++ generar_qr/script.js
@@ -9,11 +9,25 @@ document.addEventListener('DOMContentLoaded', function () {
         const text = qrText.value;
         if (text.trim() !== '') {
             // Crear un nuevo objeto QRCode
-            const qrcode = Image(new QRCode(qrCode, {
+            const qrcode = new QRCode(qrCode, {
                 text: text,
                 width: 128,
                 height: 128
-            }));
+            });
+
+            // Capturar la imagen QR con html2canvas
+            html2canvas(qrCode).then(function(canvas) {
+                // Agregar la imagen capturada al DOM
+                qrCode.innerHTML = '';
+                qrCode.appendChild(canvas);
+
+                // Agregar un enlace de descarga
+                const downloadLink = document.createElement('a');
+                downloadLink.href = canvas.toDataURL('image/png');
+                downloadLink.download = 'codigo-qr.png';
+                downloadLink.innerText = 'Descargar Imagen QR';
+                qrCode.appendChild(downloadLink);
+            });
         }
     });
 });
