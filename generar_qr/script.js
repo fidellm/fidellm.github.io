e5d46367fa3c957a3e6ed612f4c89ffb7634e509
navodel <115118635+Navodel@users.noreply.github.com>
2023-09-29 15:14:35 -0300

    se agrega un generador de imágenes qr

---
+++ generar_qr/script.js
@@ -0,0 +1,19 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const qrText = document.getElementById('qr-text');
+    const generateButton = document.getElementById('generate-button');
+    const qrCode = document.getElementById('qr-code');
+
+    generateButton.addEventListener('click', function () {
+        const text = qrText.value;
+        if (text.trim() !== '') {
+            // Crear un nuevo objeto QRCode
+            const qrcode = new QRCode(qrCode, {
+                text: text,
+                width: 128,
+                height: 128
+            });
+        }
+    });
+});
